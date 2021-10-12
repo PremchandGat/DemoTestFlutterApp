74af36cb3da01ff3a99e0a2733990a3c542de73c
Premchand <premchandg278@gmail.com>
2021-10-12 15:43:00 +0530

    If else statement

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,24 +8,40 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool like = false;
   int _count = 0;
   addCount() {
     setState(() {
+      if (like) {
+        like = false;
+      } else {
+        like = true;
+      }
       _count = _count + 1;
       print(_count);
     });
+  }
+
+  likeWidget() {
+    if (like) {
+      return Icon(Icons.favorite);
+    } else {
+      return Image.network(
+          "https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: likeWidget(),
         title: const Text("Welcome"),
       ),
       body: Center(
         child: Text(
           "Total Count = $_count",
-          style: const TextStyle(fontSize: 32),
+          style: const TextStyle(fontSize: 32, color: Colors.red),
         ),
       ),
       floatingActionButton: FloatingActionButton(
